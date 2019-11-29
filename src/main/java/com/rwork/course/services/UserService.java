5817d03e1ff131eb4a9f34bbcad1e6617403c3ca
roger28 <rogersouza635@gmail.com>
2019-11-29 19:11:44 -0300

    Exception handle - update.

--- src/main/java/com/rwork/course/services/UserService.java
+++ src/main/java/com/rwork/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.rwork.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -40,9 +42,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User user = this.repository.getOne(id);
-		updateData(user, obj);
-		return this.repository.save(user);
+		try {
+			User user = this.repository.getOne(id);
+			updateData(user, obj);
+			return this.repository.save(user);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id); 
+		}
+		
 	}
 
 	private void updateData(User user, User obj) {
