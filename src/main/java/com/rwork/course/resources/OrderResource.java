5132d0f611cd798b2e7ece63b033dc9de3312101
Roger28 <rogersouza635@gmail.com>
2019-12-22 02:48:54 -0300

    Added cross origin to AngularJS Project

--- src/main/java/com/rwork/course/resources/OrderResource.java
+++ src/main/java/com/rwork/course/resources/OrderResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.rwork.course.entities.Order;
 import com.rwork.course.services.OrderService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/orders")
 public class OrderResource {
 
--- src/main/java/com/rwork/course/resources/UserResource.java
+++ src/main/java/com/rwork/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.rwork.course.entities.User;
 import com.rwork.course.services.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/users")
 public class UserResource {
 
